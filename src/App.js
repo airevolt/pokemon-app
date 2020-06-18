c3a44b4586ce6fe8bc5d10687670914cdda91000
Brady Haden <bhaden94@yahoo.com>
2020-06-18 13:17:58 -0700

    collection working with add and remove feature

--- src/App.js
+++ src/App.js
@@ -12,14 +12,41 @@ class App extends Component {
       search: "",
       singleSearch: false,
       seeAll: false,
+      showCollection: false,
       searchedPokemon: [],
-      pokemon: []
+      pokemon: [],
+      collection: []
     };
     this.fetchPokemon();
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.getCollection = this.getCollection.bind(this);
   }
 
+  getCollection(val) {
+    let pokemon = val.currentPokemon
+    console.log(pokemon)
+    let deletePokemon = val.inCollection
+    console.log(deletePokemon)
+    // check if pokemon already in collection
+    if (deletePokemon) {
+      this.setState({collection: this.state.collection.filter(el => el.name !== pokemon.name)})
+    } else {
+      if (this.state.collection.indexOf(pokemon) === -1){
+        // add to collection
+        this.setState({collection: this.state.collection.concat(pokemon)})
+      }
+      else {
+        // pokemon already in collection
+        alert("Pokemon already in collection")
+      }
+    }
+    
+
+  }
+
+  // get pokemon data on startup
+  // 807 is currently the last pokemon in pokeapi
   async fetchPokemon() {
     for (let i=1; i<=807; i++) {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/` + i)
@@ -28,10 +55,13 @@ class App extends Component {
     }
   }
 
+  // sets our search criteria as we type in the search bar
   handleChange(event) {
     this.setState({search: event.target.value});
   }
 
+  // check if we click the search button or the reset button
+  // sets the state to with the found pokemon or outputs an alert saying not found
   handleClick(event) {
     if (event.target.value === "search") {
       // handle search to show single pokemon
@@ -49,7 +79,13 @@ class App extends Component {
     }
     else {
       // handle reset
-      this.setState({singleSearch: false, seeAll: false})
+      this.setState(
+        {
+          singleSearch: false, 
+          seeAll: false, 
+          showCollection: false
+        }
+      )
     }
     
   }
@@ -58,6 +94,35 @@ class App extends Component {
     if (!this.state.pokemon) {
       return null;
     }
+    // have we search for a pokemon?
+    let searchResults;
+    if (!this.state.singleSearch) {
+      searchResults = <div />
+    } else {
+      searchResults = <Table value={this.state.searchedPokemon} sendData={this.getCollection} />
+    }
+    // do we want to see our collection or all the pokemon
+    let collectionOrSeeAll;
+    if (!this.state.seeAll && !this.state.showCollection) {
+      collectionOrSeeAll = 
+        <div>
+          <MDBBtn color="info" onClick={() => this.setState({seeAll: true, showCollection: false})}>See All</MDBBtn>
+          <MDBBtn color="amber" onClick={() => this.setState({showCollection: true, seeAll: false})}>Show Collection</MDBBtn>
+        </div>
+    } else if (this.state.seeAll && !this.state.showCollection) {
+      collectionOrSeeAll = 
+        <div>
+          <MDBBtn color="amber" onClick={() => this.setState({showCollection: true, seeAll: false})}>Show Collection</MDBBtn> 
+          <Table value={this.state.pokemon} sendData={this.getCollection} />
+        </div>
+    } else {
+      collectionOrSeeAll = 
+        <div>
+          <MDBBtn color="info" onClick={() => this.setState({seeAll: true, showCollection: false})}>See All</MDBBtn> 
+          <Table value={this.state.collection} inCollection="true" sendData={this.getCollection} />
+        </div>
+    }
+
     return (
       <div className="App">
         <Header />
@@ -67,10 +132,8 @@ class App extends Component {
           <MDBBtn color="elegant" type="submit" value="reset" onClick={this.handleClick}>Reset</MDBBtn>
         </div>
         <div>
-          {(!this.state.singleSearch) ? <div /> 
-            : <Table value={this.state.searchedPokemon} />}
-          {(!this.state.seeAll) ? <MDBBtn color="info" onClick={() => this.setState({seeAll: true})}>See All</MDBBtn> 
-            : <Table value={this.state.pokemon}/>}
+          {searchResults}
+          {collectionOrSeeAll}
         </div>
       </div>
     );
--- src/Table.js
+++ src/Table.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import "./App.css";
 // bootstrap
-import { MDBBtn, MDBDataTable, MDBPopover, MDBPopoverBody  } from 'mdbreact';
+import { MDBBtn, MDBDataTable, MDBPopover, MDBPopoverBody, MDBTooltip } from 'mdbreact';
 import _ from 'lodash'; // to capitalize the first letter of every pokemon (yes I am lazy)
+import catchImg from "./pokeball.png";
+import releaseImg from "./openball.png";
 
 function Table(props) {
+    let buttonImg;
+    let btnColor;
+    let tooltip;
+    let isInCollection = true;
 
-
+    if (props.inCollection) {
+        buttonImg = <img src={releaseImg} />
+        btnColor = 'light'
+        tooltip = <div>Remove from Collection</div>
+    }
+    else {
+        buttonImg = <img src={catchImg} />
+        btnColor = 'amber'
+        tooltip = <div>Add to Collection</div>
+        isInCollection = false
+    }
     // data for datatable
     let data = {
         // columns for table
         columns: [
+            {
+                label: '',
+                field: 'add'
+            },
             {
                 label: 'Picture',
                 field: 'picture'
@@ -41,10 +61,20 @@ function Table(props) {
     }
     // loops over each pokemon and sets the values associated to our table into a new object
     // and then pushes that new object into the row array
+    /*
+    take e.target.id(links to index of pokemon we ant to add)
+    collection = props.value[e.target.id]
+    */
     props.value.forEach(pokemon => {
-        const name = _.startCase(pokemon.name);
-        const bulbapedia = "https://bulbapedia.bulbagarden.net/wiki/" + name;
-        let obj = {
+        const name = _.startCase(pokemon.name); // name
+        const bulbapedia = "https://bulbapedia.bulbagarden.net/wiki/" + name; // link for picture to more information
+        let pokeObj = {
+            add: <MDBTooltip placement="top">
+                    <MDBBtn id="collectionBtn" onClick={() => props.sendData({currentPokemon:pokemon, inCollection: isInCollection})} color={btnColor} size="sm">
+                        {buttonImg}
+                    </MDBBtn>
+                    <div>{tooltip}</div>
+                 </MDBTooltip>,
             picture: <a href={bulbapedia} target="_blank"><img src={pokemon.sprites.front_default} /></a>,
             id: pokemon.id,
             name: name,
@@ -66,7 +96,7 @@ function Table(props) {
                                         </MDBPopoverBody>
                                     </div>
                         </MDBPopover>,
-            // moves are shown with a bootstrap popover with li elements and a max height css property
+            // moves are shown with a bootstrap popover with li elements, a max height css property
             // and overflow auto so it is scrollable on long movesets
             moves: <MDBPopover placement="top" popover clickable id="popper1">
                             <MDBBtn color="info" size="sm">See Moveset</MDBBtn>
@@ -77,15 +107,15 @@ function Table(props) {
                                 </div>
                     </MDBPopover>,
         }
-        data.rows.push(obj)
+        data.rows.push(pokeObj) // the new pokemon obj gets put into the rows array
     })
 
     return (
         <MDBDataTable className="table"
             hover
             striped
+            sorting="false"
             data={data}
-            sorting={false}
             />
 
     );
